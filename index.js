3e94f650b36de0d12ad8ba2eae4e9443b671903c
CodeSerch <sergioenriquebg28@gmail.com>
2021-12-11 21:04:32 -0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let fadeTime = 1000;
 
-$('.titulo').hide();
+/*$('.titulo').hide();
 $( document ).ready(function() {
     console.log( "ready!" );
     $('.titulo').slideDown(fadeTime);
@@ -12,4 +12,22 @@ $("#servicios").click(() => {
       }
       topFunction();
 
-} )
+} )*/
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
